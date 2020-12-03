a8dcbcbfb265346daaff13c1b358abcb732af0a3
szglabs <sdrummond95@gmail.com>
2020-12-02 23:43:06 -0500

    basic mouseover functionality

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const gridContainer = document.querySelector('.grid-container');
+let gridSize = 25;
+
+// function to create the grid
+function createGrid(gridSize) {
+    gridContainer.style.setProperty('--grid-rows', gridSize)
+    gridContainer.style.setProperty('--grid-cols', gridSize)
+    for (let i = 0; i < (gridSize * gridSize); i++) {
+        let div = document.createElement('div');
+        div.addEventListener('mouseover', function() {
+            div.style.backgroundColor = 'black';
+        })
+        gridContainer.appendChild(div).className = 'grid-divs';
+    }
+
+}
+
+
+
+createGrid(gridSize);
